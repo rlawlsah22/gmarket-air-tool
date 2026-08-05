c06d5d5451f57eee05a0a2bbdb3b7fad833b4d6b
rlawlsah22 <rlawlsah22@gmail.com>
2026-08-05 10:20:38 +0900

    Update gmarket_air_gui.py

--- gmarket_air_gui.py
+++ gmarket_air_gui.py
@@ -17,7 +17,7 @@ import ctypes
 # ─────────────────────────────────────────────
 #  자동 업데이트 (GitHub)
 # ─────────────────────────────────────────────
-CURRENT_VERSION = "2.3"
+CURRENT_VERSION = "2.2"
 GITHUB_USER     = "rlawlsah22"
 GITHUB_REPO     = "gmarket-air-tool"
 RAW_BASE        = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/main"
